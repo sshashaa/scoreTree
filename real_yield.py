a2692319969a31308705fd0464f53a3336b6e432
ozgesurer <ozgesurer2019@u.northwestern.edu>
2022-12-02 17:03:47 -0500

    Yield data

--- real_yield.py
+++ real_yield.py
@@ -28,48 +28,38 @@ rows = rows[1:]
 for i in range(len(rows)):
     rows[i] = [float(x) for x in rows[i]]
 
-    
-is_cat = []
-cov_uniqvals = []
-x_dim = len(rows[0])-1
-for i in range(x_dim):
-    unique_vals = list(sorted(set(column(rows, i))))
-    cov_uniqvals += [unique_vals]
-    if len(unique_vals) <= 2:
-        is_cat += [1]
-    else:
-        is_cat += [0]
-            
-test_index = list(random.sample(range(len(rows)), 1000))
+
+random.seed(0)       
+test_index = list(random.sample(range(len(rows)), 10000))
 rem_index = list(set(range(len(rows))) - set(test_index))
 rem_set = [rows[index] for index in rem_index]    
 test_set_fix = [rows[index] for index in test_index]
 
 # inputs
 max_depth = 3
-min_node_size = 100
+min_node_size = 10
 num_quantiles = 20
 total_reps = 5#30 
 alpha = .2
 tol = 0
 n = 100
 
-
+    
 def OneRep(k):
-    # random.seed(k + 10)
-    # filename = 'less_noise_examples/synth7_rep_' + str(k) + '.csv'
-    # rows = pd.read_csv(filename, header=None)
-    data_title = 'yield'
-    datafile = "data/test_"+data_title+".txt"
-    # log_file = open(directory+"log_"+data_title+"_"+params+".txt", 'a+')
-            
-
-    tr_set = random.sample(rem_set, n)
-
-
-
-
+    random.seed(k)  
+    tr_set = random.sample(rem_set, 10000)[0:n]
     dataset = [tr_set, test_set_fix]
+    
+    is_cat = []
+    cov_uniqvals = []
+    x_dim = len(tr_set[0])-1
+    for i in range(x_dim):
+        unique_vals = list(sorted(set(column(tr_set, i))))
+        cov_uniqvals += [unique_vals]
+        if len(unique_vals) <= 2:
+            is_cat += [1]
+        else:
+            is_cat += [0]
     
     test_set = list()
     for row in dataset[1]:
@@ -93,7 +83,7 @@ def OneRep(k):
         for m in methods: 
             # Fit the tree model
             CARTmodel = scoreCART(m, 
-                                  rows, 
+                                  tr_set, 
                                   tol, 
                                   max_depth, 
                                   min_node_size, 
@@ -112,36 +102,43 @@ def OneRep(k):
                 d = {'Method': m, 'Metric': metr, 'Train': np.round(dict_eval[metr][1], 2), 'Test': np.round(dict_eval[metr][0], 2), 'Threshold': pr}
                 dictable.append(d)
     dfres = pd.DataFrame(dictable)
+    name = str(k) +'.txt'
+    
+    with open(r'rep' + str(k) + '.txt', 'w') as fp:
+        fp.writelines(str(dfres.values))
     return dfres
 
 # Run parallel for each replicate
 scores_reps = Parallel(n_jobs=min(total_reps, 20))(delayed(OneRep)(rep_no) for rep_no in range(total_reps))   
+#with open(r'all.txt', 'w') as fp:
+#    fp.writelines(str(scores_reps[0].values))
 
-liste = []
-for i in range(total_reps):
-    ls = scores_reps[i].copy()
-    ls['Rep'] = i
-    liste.append(ls)
-df_scores = pd.concat(liste)
-
-
-metrics = ['sse', 'crps', 'dss', 'is1']
-prune_thr_list = [0, 0.1, 0.3, 0.5, 0.8]
-
-fig, axes = plt.subplots(len(metrics), len(prune_thr_list), sharex=True, figsize=(25, 25))
-font = {'family' : 'normal',
-        'weight' : 'bold',
-        'size'   : 15}
-
-plt.rc('font', **font) 
-for metric_id, met in enumerate(metrics):
-    for th_id, th in enumerate(prune_thr_list):
-        df = df_scores[(df_scores['Metric'] == met) & (df_scores['Threshold'] == th)]
-        sns.boxplot(ax=axes[metric_id, th_id], x="Method", y="Test", data=df,  palette="Set3")
-        axes[metric_id, th_id].set_ylabel('')
-        if th == 0:
-            axes[metric_id, th_id].set_ylabel('Test' + ' (' + met + ')')
-        if metric_id == 0:
-            axes[metric_id, th_id].set_title('Threshold: ' + str(th))
-
-plt.show()
+show = False
+if show:
+    liste = []
+    for i in range(total_reps):
+        ls = scores_reps[i].copy()
+        ls['Rep'] = i
+        liste.append(ls)
+    df_scores = pd.concat(liste)
+    
+    metrics = ['sse', 'crps', 'dss', 'is1']
+    prune_thr_list = [0, 0.1, 0.3, 0.5, 0.8]
+    
+    fig, axes = plt.subplots(len(metrics), len(prune_thr_list), sharex=True, figsize=(25, 25))
+    font = {'family' : 'normal',
+            'weight' : 'bold',
+            'size'   : 15}
+    
+    plt.rc('font', **font) 
+    for metric_id, met in enumerate(metrics):
+        for th_id, th in enumerate(prune_thr_list):
+            df = df_scores[(df_scores['Metric'] == met) & (df_scores['Threshold'] == th)]
+            sns.boxplot(ax=axes[metric_id, th_id], x="Method", y="Test", data=df,  palette="Set3")
+            axes[metric_id, th_id].set_ylabel('')
+            if th == 0:
+                axes[metric_id, th_id].set_ylabel('Test' + ' (' + met + ')')
+            if metric_id == 0:
+                axes[metric_id, th_id].set_title('Threshold: ' + str(th))
+    
+    plt.show()
